88804c13812986a4d247a13261048833d0f94741
Katrina Beaty <typingforyou@yahoo.com>
2018-08-30 17:09:30 -0400

    added feature to Financial Advisor

--- src/FinancialAdvisorApp.java
+++ src/FinancialAdvisorApp.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
 
 public class FinancialAdvisorApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 
+		int moneyInBank;
+		int yearsUntilRetirement;
+
+		System.out.println("What is your first and last name?");
+
+		String firstName = input.next();
+		System.out.println(firstName);
+
+		String lastName = input.next();
+		System.out.println(lastName);
+
+		System.out.println("What is your age?");
+		String age = input.next();
+
+		System.out.println("What your birth month number.");
+		String birthMonth = input.next();
+
+		System.out.println("How many siblings do you have?");
+		String siblings = input.next();
+
+		System.out.println("What is your favorite ROYGBIV color?");
+		System.out.print("Type help, for ROYGBIV colors.");
+		String validateInput = input.next();
+
+		System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.");
+		String roygbivHelp = input.next();
+
+		{
+
+		}
 	}
-
 }
